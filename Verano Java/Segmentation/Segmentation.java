536e6ba588362440f096d74b5f58ca0ca1da3dd1
ErnoMitrovic <a01656828@itesm.mx>
2020-06-13 17:11:22 -0500

    Nueva actualización
    A raíz de la clase del 12 de junio

--- Verano Java/Segmentation/Segmentation.java
+++ Verano Java/Segmentation/Segmentation.java
@@ -1,6 +1,7 @@
-import java.util.Arrays;
+
+//import java.util.Arrays;
 import java.util.Scanner;
-import java.lang.Math;
+//import java.lang.Math;
 import java.util.ArrayList;;
 
 /*
@@ -11,14 +12,13 @@ import java.util.ArrayList;;
 //atgaataagtaa
 
 public class Segmentation {
-	/*
-	int nOfProtein;
-	String arn;
-	public Segmentation(int nOfProtein){
-		arn = "empty";
-		this.nOfProtein = nOfProtein;
-	}
-	*/ 
+	
+	// int nOfProtein;
+	// String arn;
+	// public Segmentation(int nOfProtein){
+	// 	arn = "empty";
+	// 	this.nOfProtein = nOfProtein + 1;
+	// }
 
 	public static ArrayList<String> find(String chain, final String startCodon, final String endCodon, final String
 	endCodon1, final String endCodon2) {
@@ -43,8 +43,8 @@ public class Segmentation {
 			}
 			else{
 				end = 0; end1 = 0; end2 = 0;
-				//int indexForArray = 0;
-				int flag = 0;
+				int indexForArray = 0;
+				//int flag = 0;
 				int indexStartRepeat, indexEndRepeat;
 				while (!chain.isEmpty()){
 					indexStartRepeat = 0;
@@ -61,9 +61,7 @@ public class Segmentation {
 							start = chain.lastIndexOf(startCodon, start);
 							end = chain.indexOf(endCodon, end + startCodon.length());
 							System.out.println("Start " + start + "End " + end);
-							flag++;
-							if (flag > 7) break;
-							if (indexStartRepeat == start && indexEndRepeat == end) break;
+							if (indexStartRepeat == start || indexEndRepeat == end) break;
 							indexEndRepeat = end;
 							indexStartRepeat = start;
 						}
@@ -79,9 +77,7 @@ public class Segmentation {
 							end1 = chain.indexOf(endCodon1, end1 + endCodon1.length());
 							System.out.println("Start " + start);
 							System.out.println("Start " + start + " End1 " + end1);
-							flag++;
-							if (flag > 7) break;
-							if (indexStartRepeat == start && indexEndRepeat == end1) break;
+							if (indexStartRepeat == start || indexEndRepeat == end1) break;
 							indexEndRepeat = end1;
 							indexStartRepeat = start;
 						}
@@ -96,9 +92,7 @@ public class Segmentation {
 							start = chain.lastIndexOf(startCodon, start);
 							end2 = chain.indexOf(endCodon2, end2 + startCodon.length());
 							System.out.println("Start " + start + " End2 " + end2);
-							flag++;
-							if (flag > 7) break;
-							if (indexStartRepeat == start && indexEndRepeat == end2) break;
+							if (indexStartRepeat == start || indexEndRepeat == end2) break;
 							indexEndRepeat = end2;
 							indexStartRepeat = start;
 						}
@@ -107,8 +101,8 @@ public class Segmentation {
 						//aindexForArray++;
 						chain = chain.replace(protein, "");
 					}
-					if (flag == 4) break;
-					flag++;
+					if (proteins.get(indexForArray).equals("at")) proteins.remove(indexForArray);
+					indexForArray++;
 				}
 				//System.out.println(protein);
 				/*
@@ -121,9 +115,80 @@ public class Segmentation {
 			return proteins;
 		}
 	}
+
+	public static int longestGene(String [] chains){
+			int lengthOfGene = chains[0].length();
+			if (lengthOfGene == -1) return lengthOfGene;
+			else{
+				for (String gene : chains) {
+					if (lengthOfGene < gene.length()) lengthOfGene = gene.length();
+				}
+			}
+			System.out.println("The largest gene has a length of: " + lengthOfGene);
+			return lengthOfGene;
+		}
+
+	public static int proteinsLengthOver (String [] chains, final int majorLength){
+		int count = 0;
+		if (!chains[0].isEmpty()){
+			for (String protein : chains) {
+				if (protein.length() > majorLength) count++;
+			}
+		}
+		System.out.println("The number of proteins that have a length of more than " + majorLength + " are: " + count);
+		return count;
+	}
+
+	public static String[] GetStringArray(ArrayList<String> arr) {
+
+		// declaration and initialise String Array
+		String str[] = new String[arr.size()];
+
+		// Convert ArrayList to object array
+		Object[] objArr = arr.toArray();
+
+		// Iterating and converting to String
+		int i = 0;
+		for (Object obj : objArr) {
+			str[i++] = (String) obj;
+		}
+
+		return str;
+	}
+
+	public static int countInString(String stringToFind, String stringFinding) {
+		stringToFind = stringToFind.toLowerCase();
+		stringFinding = stringFinding.toLowerCase();
+		int count = 0;
+		boolean found = stringFinding.contains(stringToFind);
+		if (found) {
+			int index = stringFinding.indexOf(stringToFind);
+			while (index != -1) {
+				count++;
+				stringFinding = stringFinding.substring(index + 1);
+				index = stringFinding.indexOf(stringToFind);
+			}
+			return count;
+		} else
+			return count;
+	}
+
+	public static float ratioProtein(String[] chains, final String firstToExamine, final String lastToExamine){
+		float value = 0f;
+		String wholeText;
+		if (chains[0].length() >= 0){
+			wholeText = "";
+			for (String gen : chains) {
+				wholeText.concat(gen);
+			}
+			int times = countInString(firstToExamine, wholeText) + countInString(lastToExamine, wholeText);
+			value = times / (float)wholeText.length();
+		} 
+		System.out.println("The ratio of appearance in the text is: " + value);
+		return value;
+	}
 	public static void main(final String[] args){
 		Scanner sc = new Scanner(System.in);
-		//ArrayList<Segmentation> proteins = new ArrayList<>();
 		//Segmentation protein = new Segmentation(1);
 		System.out.println("Insert chain: ");
 		String chain = sc.next();
@@ -131,33 +196,30 @@ public class Segmentation {
 		final String endCodon = "taa";
 		final String endCodon1 = "tag";
 		final String endCodon2 = "tga";
+
+		System.out.println("Enter the major index to find: ");
+		final int majorLength = sc.nextInt();
 		sc.close();
 		//String empty = chain.replace(chain, "");
-		System.out.println(find(chain, startCodon, endCodon,endCodon1,endCodon2));
 
-		/*
-		proteins.add(protein);
-		proteins.get(0).arn = find(chain,startCodon,endCodon);
-		if (chain.substring(lastIndex(chain,startCodon,endCodon), chain.length()).length() < startCodon.length()*2){
-			System.out.println("The protein is " + proteins.get(0).arn);
-		}
-		*/
+		ArrayList <String> arrayList = find(chain, startCodon, endCodon, endCodon1, endCodon2);
+		System.out.println(arrayList);
+		String [] chains = GetStringArray(arrayList);
+
+		longestGene(chains);
+
+		proteinsLengthOver(chains, majorLength);
+
+		ratioProtein(chains, "a", "t");
+
+		// int sizeOfList = find(chain, startCodon, endCodon, endCodon1, endCodon2).size();
+
+		// Segmentation [] chains = new Segmentation[sizeOfList];
+		// for(int i = 0; i < sizeOfList; i++){
+		// 	Segmentation protein = new Segmentation(i);
+		// 	protein.arn = find(chain, startCodon, endCodon, endCodon1, endCodon2).get(i);
+		// 	chains[i] = protein;
+		// 	System.out.println("The " + chains[i].nOfProtein + "° is " + chains[i].arn);
+		// }
 	}
-	/*
-	protected static int lastIndex(final String chain, final String startCodon, final String endCodon){
-		final int start = chain.indexOf(startCodon);
-		int lastInd = -1;
-		if (start < 0) return -1;
-		else {
-			lastInd = chain.indexOf(endCodon, start + startCodon.length());
-			String protein = chain.substring(start, lastInd + endCodon.length());
-			while (protein.length() % 3 != 0 && lastInd > 0){
-				lastInd = chain.indexOf(endCodon, lastInd + endCodon.length());
-				protein = chain.substring(start, lastInd + endCodon.length());
-				if (protein.length() % 3 != 0) lastInd = -1;
-			}
-		}
-		return lastInd;
-	}
-	*/
 }
